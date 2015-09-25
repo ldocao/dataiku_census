4daef056d2780aef1bd3119bba6e5a13752ab3c3
ldocao <ldocao@gmail.com>
2015-09-25 18:32:42 +0200

    make histogram for all variables

--- exploratory/stats.py
+++ exploratory/stats.py
@@ -2,11 +2,24 @@ import numpy as np
 import pandas as pd
 import load_data as ld
 import ipdb
-
+import matplotlib.pyplot as plt
 
 training_file = "../data/census_income_learn.csv"
 metadata_file = "../data/census_income_metadata.txt"
 
-result = ld.prepare_dataframe(training_file, metadata_file=metadata_file)
+df = ld.prepare_dataframe(training_file, metadata_file=metadata_file)
+nrows = len(df)
 
+for c in df.columns.values:
+    print c
+    plt.figure()
+    try:
+        df[c].hist(orientation="horizontal")
+    except TypeError:
+        df[c].value_counts().plot(kind='barh') 
 
+    plt.axvline(nrows,linestyle="dashed",color="black")
+    plt.xlim([0,nrows])
+    plt.suptitle(c)
+    plt.tight_layout()
+    plt.savefig("./figures/"+c+".pdf")
