9a8713f36575f2fc95fb90b918165c4b617a57d9
ldocao <ldocao@gmail.com>
2015-09-28 18:13:07 +0200

    content: first fit to ["age", "instance weight"] space
    behave as expected

--- learning/logistic_regression.py
+++ learning/logistic_regression.py
@@ -9,37 +9,36 @@ from sklearn.linear_model import LogisticRegression
 training_file = "../data/census_income_learn.csv"
 metadata_file = "../data/census_income_metadata.txt"
 df = ld.prepare_dataframe(training_file, metadata_file=metadata_file)
-
 df = sel.bias_population(df,1.)
 
 
 
-# df_subset = df[["age", "instance weight", "incomes classification"]]
-# incomes = df_subset["incomes classification"]
+df_subset = df[["age", "instance weight", "incomes classification"]]
+incomes = df_subset["incomes classification"]
 
-# trues = df_subset[['age', 'instance weight']][incomes][:2000]
-# falses = df_subset[['age', 'instance weight']][~incomes][:2000]
-# plt.figure()
-# pd.set_option('display.mpl_style', 'default')
-# ax = plt.axes()
-# trues.plot(x='age', y='instance weight', kind='scatter', ax=ax, alpha=0.3, label='True')
-# falses.plot(x='age', y='instance weight', kind='scatter', color='orange', ax=ax, alpha=0.3, label='False')
-# plt.suptitle("Training set")
-# plt.show()
+trues = df_subset[['age', 'instance weight']][incomes][:2000]
+falses = df_subset[['age', 'instance weight']][~incomes][:2000]
+plt.figure()
+pd.set_option('display.mpl_style', 'default')
+ax = plt.axes()
+trues.plot(x='age', y='instance weight', kind='scatter', ax=ax, alpha=0.3, label='True')
+falses.plot(x='age', y='instance weight', kind='scatter', color='orange', ax=ax, alpha=0.3, label='False')
+plt.suptitle("Training set")
+plt.show()
 
 
 
-# cls = LogisticRegression(C=0.01) #define classifier
-# features = df_subset[['age', 'instance weight']]
-# cls.fit(features, incomes)
-# predictions = cls.predict(features)
+cls = LogisticRegression() #define classifier
+features = df_subset[['age', 'instance weight']]
+cls.fit(features, incomes)
+predictions = cls.predict(features)
 
-# trues = df_subset[['age', 'instance weight']][predictions][:2000]
-# falses = df_subset[['age', 'instance weight']][~predictions][:2000]
-# plt.figure()
-# ax = plt.axes()
-# pd.set_option('display.mpl_style', 'default')
-# #trues.plot(x='age', y='instance weight', kind='scatter', ax=ax, alpha=0.5, label='True')
-# falses.plot(x='age', y='instance weight', kind='scatter', color='orange', ax=ax, alpha=0.5, label='False')
-# plt.suptitle("Prediction")
-# plt.show()
+trues = df_subset[['age', 'instance weight']][predictions][:2000]
+falses = df_subset[['age', 'instance weight']][~predictions][:2000]
+plt.figure()
+ax = plt.axes()
+pd.set_option('display.mpl_style', 'default')
+trues.plot(x='age', y='instance weight', kind='scatter', ax=ax, alpha=0.5, label='True')
+falses.plot(x='age', y='instance weight', kind='scatter', color='orange', ax=ax, alpha=0.5, label='False')
+plt.suptitle("Prediction")
+plt.show()
