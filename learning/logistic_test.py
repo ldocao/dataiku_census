b675c17898f86c1217b042967bb10a4961788cc5
ldocao <ldocao@gmail.com>
2015-09-28 16:57:47 +0200

    feat: test finished

--- learning/logistic_test.py
+++ learning/logistic_test.py
@@ -1,20 +1,67 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import confusion_matrix
+
+import ipdb
 
 
-n_points = 1000
+
+def scatter_two_categories(cat1, cat2):
+    pd.set_option('display.mpl_style', 'default')
+    ax = plt.axes()
+    cat1.plot(x='x', y='y', kind='scatter', ax=ax, alpha=0.6, label='True')
+    cat2.plot(x='x', y='y', kind='scatter', color='orange', ax=ax, alpha=0.6, label='False')
+    return ax
+
+
+##PREPARE DATA
+### features
+n_points = 100000
+np.random.seed(0)
 df = pd.DataFrame({'x': np.random.normal(0, 1, n_points),
                    'y': np.random.normal(0, 1, n_points)})
 
-df["v"] = 2*df.x - df.y
-probabilities = 1 / (1 + np.exp(-1 * df.v))
-
-x = - 1/3 * (df['x'] + df['y'])
+### target classification
+x = - 2./3 * (df['x'] + df['y'])
 probabilities = 1 / (1 + np.exp(-1 * x))
 df['target'] = np.random.uniform(0, 1, n_points) < probabilities
+target = df['target']
 
 
+###subselect sample to plot
+trues = df[['x', 'y']][target][:500]
+falses = df[['x', 'y']][~target][:500]
 plt.figure()
-df.plot(kind='scatter', x='x', y='y', c="target");
+pd.set_option('display.mpl_style', 'default')
+ax = plt.axes()
+trues.plot(x='x', y='y', kind='scatter', ax=ax, alpha=0.6, label='True')
+falses.plot(x='x', y='y', kind='scatter', color='orange', ax=ax, alpha=0.6, label='False')
+plt.suptitle("Training set")
 plt.show()
+
+
+
+## LOGISTIC REGRESSION
+cls = LogisticRegression() #define classifier
+features = df[['x', 'y']]
+features_train, features_test, target_train, target_test = train_test_split(features, df['target'])
+cls.fit(features_train, target_train)
+predictions = cls.predict(features_test)
+
+df_test = features_test
+df_test["predictions"] = predictions
+trues = df_test[['x', 'y']][predictions][:500]
+falses = df_test[['x', 'y']][~predictions][:500]
+plt.figure()
+ax = plt.axes()
+pd.set_option('display.mpl_style', 'default')
+trues.plot(x='x', y='y', kind='scatter', ax=ax, alpha=0.6, label='True')
+falses.plot(x='x', y='y', kind='scatter', ax=ax, color='orange', alpha=0.6, label='False')
+plt.suptitle("Validation set")
+plt.show()
+
+
+print "finished"
