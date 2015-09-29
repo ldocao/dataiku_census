e8549862a01849e2d665f30470a469d45030c49d
ldocao <ldocao@gmail.com>
2015-09-29 16:37:41 +0200

    feat: condense operation to return only the prediction

--- learning/logistic_regression.py
+++ learning/logistic_regression.py
@@ -1,44 +1,28 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import load_data as ld
-import selection as sel
+import feature_engineering as feat
 from sklearn.linear_model import LogisticRegression
-
-training_file = "../data/census_income_learn.csv"
-metadata_file = "../data/census_income_metadata.txt"
-df = ld.prepare_dataframe(training_file, metadata_file=metadata_file)
-df = sel.bias_population(df,1.)
+from constants import *
 
 
 
-df_subset = df[["age", "instance weight", "incomes classification"]]
-incomes = df_subset["incomes classification"]
+def predict(df):
+    """Return prediction of incomes classification"""
 
-trues = df_subset[['age', 'instance weight']][incomes][:2000]
-falses = df_subset[['age', 'instance weight']][~incomes][:2000]
-plt.figure()
-pd.set_option('display.mpl_style', 'default')
-ax = plt.axes()
-trues.plot(x='age', y='instance weight', kind='scatter', ax=ax, alpha=0.3, label='True')
-falses.plot(x='age', y='instance weight', kind='scatter', color='orange', ax=ax, alpha=0.3, label='False')
-plt.suptitle("Training set")
-plt.show()
+    ## load data
+    df = ld.prepare_dataframe(TRAINING_FILE, metadata_file=METADATA_FILE)
+    df = feat.engineer_dataframe(df)
+
+    ## machine learning: logistic regression
+    cls = LogisticRegression() #define classifier
+    features = df.drop(PREDICTION_COLNAME, axis=1)
+    target = df[PREDICTION_COLNAME]
+    cls.fit(features, target)
+    predictions = cls.predict(features)
+    
+    return predictions
 
 
 
-cls = LogisticRegression() #define classifier
-features = df_subset[['age', 'instance weight']]
-cls.fit(features, incomes)
-predictions = cls.predict(features)
 
-trues = df_subset[['age', 'instance weight']][predictions][:2000]
-falses = df_subset[['age', 'instance weight']][~predictions][:2000]
-plt.figure()
-ax = plt.axes()
-pd.set_option('display.mpl_style', 'default')
-trues.plot(x='age', y='instance weight', kind='scatter', ax=ax, alpha=0.5, label='True')
-falses.plot(x='age', y='instance weight', kind='scatter', color='orange', ax=ax, alpha=0.5, label='False')
-plt.suptitle("Prediction")
-plt.show()
