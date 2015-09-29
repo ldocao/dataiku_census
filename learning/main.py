f5c27caa45d62ae3f9a313ab64208aa6760f982e
ldocao <ldocao@gmail.com>
2015-09-29 17:04:02 +0200

    fix: validation set must have same features than training set, but no bias in sampling population

--- learning/main.py
+++ learning/main.py
@@ -32,8 +32,11 @@ method = "logistic_regression"
 ## LOAD DATA
 df_train = ld.prepare_dataframe(TRAINING_FILE, metadata_file=METADATA_FILE)
 df_train = feat.engineer_dataframe(df_train)
+
+##should make sure here that df_valid has the same features than the training set
 df_valid = ld.prepare_dataframe(VALIDATION_FILE, metadata_file=METADATA_FILE)
-df_valid = feat.engineer_dataframe(df_valid)
+df_valid = df_valid[["age", "education", "sex", "num persons worked for employer", PREDICTION_COLNAME]]
+df_valid = feat.dummify(df_valid, "education")  
 
 
 ## LEARNING
