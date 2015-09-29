31525008b565fd1b2a624f0c2a38ca9051922e6b
ldocao <ldocao@gmail.com>
2015-09-29 09:09:22 +0200

    feat: add is_numerical method to pd.Series

--- learning/utils.py
+++ learning/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import random
+from constants import *
 
 def replace_character(serie, c, r):
     """Return the same dataframe where "c" is replaced by "r" in colname
@@ -34,3 +35,11 @@ def sample_random(df, n, seed=0):
     """
     random.seed(0)
     return df.ix[random.sample(df.index, n)]
+
+
+
+def is_numeric(self):
+    """Is pd.Series a numerical variable"""
+    return self.dtypes in NUMERICS
+
+setattr(pd.Series, 'is_numeric', is_numeric)
