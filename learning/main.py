d878c30b78dc658874731dfed53fb830cf96c896
ldocao <ldocao@gmail.com>
2015-09-29 17:27:01 +0200

    feat: get a proper score

--- learning/main.py
+++ learning/main.py
@@ -4,9 +4,13 @@ import load_data as ld
 import visualize
 import feature_engineering as feat
 from constants import *
+from sklearn.metrics import confusion_matrix
 import ipdb
 
-def machine_learning(df_train, df_valid, method="logistic_regression"):
+
+
+
+def machine_learning(df_train, df_valid, method=METHOD):
     """Return dataframe with prediction
 
 
@@ -26,21 +30,32 @@ def machine_learning(df_train, df_valid, method="logistic_regression"):
 
 
 
-## PARAMETERS
-method = "logistic_regression"
+def print_score(y_true, y_valid):
+    confusion_score = confusion_matrix(y_true, y_valid)
+    print confusion_score
+    print "Score summary: ", round(float(np.trace(confusion_score))/n_valid, 3)*100., "%"
+    # read confusion matrix as follows:
+    # (expected true, predicted true) (expected true, predicted false)
+    # (expected false, predicted true) (expected false, predicted false)
+
 
 ## LOAD DATA
+print "loading data..."
 df_train = ld.prepare_dataframe(TRAINING_FILE, metadata_file=METADATA_FILE)
 df_train = feat.engineer_dataframe(df_train)
 
-##should make sure here that df_valid has the same features than the training set
+###should make sure here that df_valid has the same features than the training set
 df_valid = ld.prepare_dataframe(VALIDATION_FILE, metadata_file=METADATA_FILE)
 df_valid = df_valid[["age", "education", "sex", "num persons worked for employer", PREDICTION_COLNAME]]
-df_valid = feat.dummify(df_valid, "education")  
+df_valid = feat.dummify(df_valid, "education") 
+n_valid = len(df_valid) 
 
 
 ## LEARNING
-prediction = machine_learning(df_train, df_valid.drop(PREDICTION_COLNAME), method=method)
+print "training: "+method+" ..."
+prediction = machine_learning(df_train, df_valid.drop(PREDICTION_COLNAME, axis=1), method=method)
 
-## PLOT CONTROL
+## MEASURE OF SUCCESS, PLOT CONTROL
+print_score(df_valid[PREDICTION_COLNAME].values, prediction)
+
 #visualize.compare_results(prediction, validation)
