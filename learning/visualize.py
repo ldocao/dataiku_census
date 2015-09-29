0a029a759cc7cd4e9c8c50a8ebb7895fbd3a9c9b
ldocao <ldocao@gmail.com>
2015-09-29 09:10:04 +0200

    feat: map categorical to numerical variables

--- learning/visualize.py
+++ learning/visualize.py
@@ -1,6 +1,31 @@
 ##function which project onto a 2D plane with name of two axis as arguments to plot the difference between prediction and validation
 import pandas as pd
 import numpy as np
+from constants import *
+import utils
+import matplotlib.pyplot as plt
+import ipdb
+
+
+
+
+
+def _map_categorical(x):
+    """Return a dictionary between categorical and numerical variable
+
+    Parameters:
+    ----------
+    x: pd.Series
+
+    Output:
+    ------
+    result: dict
+        map between categorical to numerical variable
+    """
+    alpha = np.unique(x) #x is any type
+    num = pd.Series(range(len(alpha)), index=alpha) #numerical equivalent to set
+    return num.to_dict()
+
 
 def _categorical_to_numerical(x):
     """Return a numerical conversion as pd.Series from categorical
@@ -13,9 +38,9 @@ def _categorical_to_numerical(x):
     ------
     result: pd.Series
     """
-
-    alpha = set(x) #any type
+    alpha = np.unique(x) #x is any type
     num = pd.Series(range(len(alpha)), index=alpha) #numerical equivalent to set
+    ipdb.set_trace()
     return x.map(num)
 
 
@@ -28,6 +53,10 @@ def scatter(x, y):
     x, y : pd.Series
     """
 
+    if not x.is_numeric(): x = _categorical_to_numerical(x)
+    if not y.is_numeric(): y = _categorical_to_numerical(y)
+
+    df = pd.DataFrame()
     df["x"] = x
     df["y"] = y
 
@@ -36,3 +65,15 @@ def scatter(x, y):
     ax = plt.axes()
     df.plot(x='x', y='y', kind='scatter', ax=ax, alpha=0.6)
     plt.show()
+
+
+
+
+
+
+
+
+
+
+
+    
