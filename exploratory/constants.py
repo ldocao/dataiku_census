8bf439e58cf524a6b74b8430c5a4b11c5745aa04
ldocao <ldocao@gmail.com>
2015-10-02 15:04:40 +0200

    feat: replace by the one from learning

--- exploratory/constants.py
+++ exploratory/constants.py
@@ -2,3 +2,12 @@ import numpy as np
 
 UNKNOWN = np.nan
 PREDICTION_COLNAME = "incomes classification"
+NUMERICS = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+
+
+TRAINING_FILE = "../data/census_income_learn.csv"
+METADATA_FILE = "../data/census_income_metadata.txt"
+VALIDATION_FILE = "../data/census_income_test.csv" 
+
+SELECTOR = "variance_threshold"
+CLASSIFIER = "random_forest"
