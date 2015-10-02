ef9547cd1ae9b83119b56c0645751f2b5c2a5e82
ldocao <ldocao@gmail.com>
2015-10-02 16:38:29 +0200

    feat: pair of plot numerical variables only

--- exploratory/pairwise.py
+++ exploratory/pairwise.py
@@ -17,3 +17,21 @@ plt.figure(figsize=figsize, dpi=dpi)
 axes = pd.tools.plotting.scatter_matrix(df, alpha=0.02, figsize=figsize)
 plt.tight_layout()
 plt.savefig('./figures/pairwise.png', dpi=dpi)
+
+
+
+## list numerical vs categorical variables
+colnames =  df.columns.values
+is_numerical = np.array([df[c].is_numeric() for c in colnames])
+is_categorical = np.logical_not(is_numerical)
+numerical_variables = list(colnames[is_numerical])
+numerical_variables.remove("detailed industry recode")
+numerical_variables.remove("detailed occupation recode")
+
+
+dpi=150
+figsize=(64, 48)
+plt.figure(figsize=figsize, dpi=dpi)
+axes = pd.tools.plotting.scatter_matrix(df[numerical_variables], alpha=0.02, figsize=figsize)
+plt.tight_layout()
+plt.savefig('./figures/pairwise_numerical.png', dpi=dpi)
